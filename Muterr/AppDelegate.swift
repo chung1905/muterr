bdf47268cf93f706f1dd539ba2b6c54c9e23ce47
chung <me@chungn.com>
2023-11-15 17:46:34 +0700

    first working version

--- Muterr/AppDelegate.swift
+++ Muterr/AppDelegate.swift
@@ -10,11 +10,23 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet var window: NSWindow!
+    @IBOutlet var window: NSNull!
 
+    var statusBarItem: NSStatusItem!
+    private var isMuted = false
+    private var currentVolume = 50
+    private let micImg = NSImage(named: NSImage.touchBarAudioInputTemplateName)
+    private let muteMicImg = NSImage(named: NSImage.touchBarAudioInputMuteTemplateName)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        NSApp.setActivationPolicy(.prohibited)
+        statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+
+        if let button = statusBarItem.button {
+            button.image = micImg
+            button.action = #selector(toggleMute)
+            button.sendAction(on: [.leftMouseUp])
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -25,6 +37,35 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         return true
     }
 
+    @objc func toggleMute() {
+        isMuted.toggle()
+        statusBarItem.button?.image = isMuted ? muteMicImg : micImg
+        currentVolume = max(currentVolume, getCurrentVolume()) // Save current volume
+        setVolume(volume: isMuted ? 0 : currentVolume)
+    }
 
+    func getCurrentVolume() -> Int {
+        var ret = 50
+        let setInputVolume = "return input volume of (get volume settings)"
+        var error: NSDictionary?
+        if let scriptObject = NSAppleScript(source: setInputVolume) {
+            if let outputString = scriptObject.executeAndReturnError(&error).stringValue {
+                ret = Int(outputString) ?? ret
+            }
+        }
+        
+        return ret <= 1 ? 0 : ret // if current = 0 or 1, return 0
+    }
+
+    func setVolume(volume: Int) {
+        let scriptSource = """
+            set volume input volume \(volume)
+        """
+
+        if let script = NSAppleScript(source: scriptSource) {
+            var error: NSDictionary?
+            script.executeAndReturnError(&error)
+        }
+    }
 }
 
